23f6c8c25b6ea7757e76a6c6043762f812a4a717
Adrián Espejo <adri.espejo@gmail.com>
2020-03-27 17:34:22 +0100

    parametrized test

--- test/test_reader.py
+++ test/test_reader.py
@@ -1,6 +1,8 @@
 import logging
 from datetime import datetime
 
+import pytest
+
 from src.reader import header_parser
 
 logging.basicConfig(level=logging.DEBUG)
@@ -22,7 +24,20 @@ def compare_trace_metadata(trace_a, trace_b):
     return False
 
 
-def test_header_parser():
-    header = "#Paraver (17/02/2020 at 11:37):1857922_ns:1(4):1:2(2:1,2:1)"
-    parsed_header = (1857922, datetime.strptime("17/02/2020 11:37", "%d/%m/%Y %H:%M"), [[4]], [[[1, 1], [1, 1]]])
-    assert parsed_header == header_parser(header)
+# esto ejecutara test_header_parser 2 veces, una con el primer #Paraver y otra con el segundo, asi puedes stackear
+# tests iguales con diferentes inputs para probar muchos casos de la misma funcion
+@pytest.mark.parametrize(
+    "header,expected_header",
+    (
+        (
+            "#Paraver (17/02/2020 at 11:37):1857922_ns:1(4):1:2(2:1,2:1)",
+            (1857922, datetime.strptime("17/02/2020 11:37", "%d/%m/%Y %H:%M"), [[4]], [[[1, 1], [1, 1]]]),
+        ),
+        (
+            "#Paraver (17/02/2020 at 11:37):1857922_ns:1(4):1:2(2:1,2:1)",
+            (1857922, datetime.strptime("17/02/2020 11:37", "%d/%m/%Y %H:%M"), [[4]], [[[1, 1], [1, 1]]]),
+        ),
+    ),
+)
+def test_header_parser(header, expected_header):
+    assert expected_header == header_parser(header)
