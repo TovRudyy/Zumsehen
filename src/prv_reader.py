e30d91a4d9d240f6c29c7f5888457f59f19aaf1b
Christoph Niethammer <niethammer@hlrs.de>
2020-04-01 12:31:45 +0200

    Out comment logging in body parser main loop part to improve performance

--- src/prv_reader.py
+++ src/prv_reader.py
@@ -79,31 +79,31 @@ def load_prv(tracefile):
             continue
         record = line.strip().split(':')
         record_type = record[0]
-        logging.debug("line to parse: %s", line.strip())
-        logging.info('record type: %s', record_type)
+        #logging.debug("line to parse: %s", line.strip())
+        #logging.debug('record type: %s', record_type)
         if record_type == '1':
             # state record
             # 1:cpu_id:appl_id:task_id:thread_id:begin_time:end_time:state
             _, cpu_id, appl_id, task_id, thread_id, begin_time, end_time, state = record
-            logging.debug("%s "*7, cpu_id, appl_id, task_id, thread_id, begin_time, end_time, state)
+            #logging.debug("%s "*7, cpu_id, appl_id, task_id, thread_id, begin_time, end_time, state)
         elif record_type == '2':
             # event record
             _, cpu_id, appl_id, task_id, thread_id, time, *events = record
-            logging.debug("%s "*5, cpu_id, appl_id, task_id, thread_id, time)
+            #logging.debug("%s "*5, cpu_id, appl_id, task_id, thread_id, time)
             event_iter = iter(events)
             for event_type in event_iter:
                 event_value = next(event_iter)
-                logging.debug("%s %s", event_type, event_value)
+                #logging.debug("%s %s", event_type, event_value)
         elif record_type == '3':
             # communication record
             _, \
             cpu_send_id, ptask_send_id, task_send_id, thread_send_id, lsend, psend, \
             cpu_recv_id, ptask_recv_id, task_recv_id, thread_recv_id, lrecv, precv, \
             size, tag = record
-            logging.debug("%s "*14,
-                    cpu_send_id, ptask_send_id, task_send_id, thread_send_id, lsend, psend,
-                    cpu_recv_id, ptask_recv_id, task_recv_id, thread_recv_id, lrecv, precv,
-                    size, tag)
+            # logging.debug("%s "*14,
+            #         cpu_send_id, ptask_send_id, task_send_id, thread_send_id, lsend, psend,
+            #         cpu_recv_id, ptask_recv_id, task_recv_id, thread_recv_id, lrecv, precv,
+            #         size, tag)
         else:
             logging.warning('skipping record of unknown type "%s"', record_type)
             continue
